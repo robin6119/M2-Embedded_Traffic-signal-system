0baa4cdfc5e884068a88a75bf5d78256e2bf4ece
robin6119 <94118694+robin6119@users.noreply.github.com>
2021-12-02 13:54:53 +0530

    Update adc.h

--- 3_Implementation/inc/adc.h
+++ 3_Implementation/inc/adc.h
@@ -1,1 +1,20 @@
+#ifndef ADC_H_
+#define ADC_H_
 
+#include "traffic-signal-system.h"
+
+/**
+ * @brief Function to initialise the analog to digital converter (ADC)
+ * 
+ */
+void adc_init();
+
+/**
+ * @brief Funciton to read analog pin and output it to a register ADC which has two 8 bit registers ADCL and ADCH.
+ * 
+ * @param channel ADC channel on which conversion must be done and value must be read
+ * @return uint16_t ADC value
+ */
+uint16_t adc_read(uint8_t channel);
+
+#endif
